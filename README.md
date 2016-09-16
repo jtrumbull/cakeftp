f5c2bc334c43cdbe7aa19a969ef7b6dfaf458f1c
Joe Trumbull <trumbull.j@gmail.com>
2016-09-16 19:11:09 -0400

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# CakeFTP
+FTP Component for CakePHP 3
+
+## Installation
+
+You can install this plugin into your CakePHP application using [composer](http://getcomposer.org).
+
+The recommended way to install composer packages is:
+
+```
+composer require jtrumbull/cakeftp
+```
---
+++ src/Controller/Component/FtpComponent.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ |------------------------------------------------------------------------------
+ | CakeFTP\Controller\Component: Ftp component
+ |------------------------------------------------------------------------------
+ */
+
+namespace CakeFTP\Controller\Component;
+
+use Cake\Controller\Component;
+
+class FtpComponent extends Component {
+  
+  
+  
+}
